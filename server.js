99c2b5e764565e7874bb91cec0aa6c6405cf71da
Yuchen Zhang <yuchen@Yuchens-MacBook-Pro.local>
2018-12-21 17:23:15 -0500

    set port

--- server.js
+++ server.js
@@ -44,6 +44,8 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+server.listen(port, () => {
+  console.log("App is running on port " + port);
+});
